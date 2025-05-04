9efe74f45a3a92202a4c66214692e44433741c8c
Madusanka Prabath <imm.prabath@gmail.com>
2025-05-04 19:31:38 -0230

    new file

--- SampleGlowing/ContentView.swift
+++ SampleGlowing/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rotation:CGFloat = 0.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .frame(width: 260, height: 340)
+                .foregroundColor(.black)
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .frame(width: 130, height: 500)
+                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.blue, .red]), startPoint: .top, endPoint: .bottom))
+                .rotationEffect(.degrees(rotation))
+                .mask {
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                        .stroke(lineWidth: 7)
+                        .frame(width: 256, height: 336)
+                }
+            
+                
         }
-        .padding()
+        .onAppear {
+            withAnimation(
+                .linear(duration: 4).repeatForever(autoreverses: false)) {
+                    rotation = 360
+                }
+        }
+        
     }
 }
 
