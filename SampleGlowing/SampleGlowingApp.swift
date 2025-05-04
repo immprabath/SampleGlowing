f78c7b124c1ef9477d54ec429c0305bff80341cd
Madusanka Prabath <imm.prabath@gmail.com>
2025-05-04 19:15:27 -0230

    Initial Commit

---
+++ SampleGlowing/SampleGlowingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleGlowingApp.swift
+//  SampleGlowing
+//
+//  Created by Madusanka I M on 2025-05-04.
+//
+
+import SwiftUI
+
+@main
+struct SampleGlowingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleGlowingTests/SampleGlowingTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleGlowingTests.swift
+//  SampleGlowingTests
+//
+//  Created by Madusanka I M on 2025-05-04.
+//
+
+import Testing
+@testable import SampleGlowing
+
+struct SampleGlowingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
